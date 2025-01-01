78ad8c461840dace40f5d6f6bc9d2069b84ed826
bergeet <hogbergelliot@gmail.com>
2025-01-01 02:44:38 +0100

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,20 @@
 import { UserTables } from "@/components/UserTables/UserTables";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getUsers } from "./actions/checkins";
+import Image from "next/image";
 
 export default async function Home() {
   const users = await getUsers();
 
   return (
-    <div id="main" className="flex flex-col  min-h-screen py-2 m-20">
+    <div id="main" className="flex flex-col min-h-screen py-2 m-20">
+      <Image
+        className="bg-image"
+        alt=""
+        src="/mount.png"
+        fill={true}
+        style={{ opacity: 0.1 }}
+      />
       <div className="flex flex-col gap-18 ">
         <Tabs defaultValue={users[0].id}>
           <TabsList className="flex flex-row w-full gap-8 md-8">
